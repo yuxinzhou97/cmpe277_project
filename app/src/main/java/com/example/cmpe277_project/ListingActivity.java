e01c8113d8c28c25092b7c176f15cfc7f3bbf7c2
Yuxin Zhou <yuxinzhou1997@gmail.com>
2021-05-05 23:25:49 -0700

    Update ListingActivity.java

--- app/src/main/java/com/example/cmpe277_project/ListingActivity.java
+++ app/src/main/java/com/example/cmpe277_project/ListingActivity.java
@@ -3,15 +3,19 @@ package com.example.cmpe277_project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +25,9 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -28,6 +35,8 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ListingActivity extends AppCompatActivity {
 
@@ -40,10 +49,14 @@ public class ListingActivity extends AppCompatActivity {
     private EditText name_product, description_product, price_product;
     private String downloadLink;
 
+
     Uri FilePathUri;
     StorageReference storageReference;
     DatabaseReference databaseReference;
     int Image_Request_Code = 7;
+    String currentUser;
+
+    CircularProgressIndicator circular_indicator;
 
 
     @Override
@@ -51,7 +64,10 @@ public class ListingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing);
 
+        circular_indicator = findViewById(R.id.circular_indicator);
+        circular_indicator.setVisibility(View.INVISIBLE);
 
+        currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
         btn_upload_product = findViewById(R.id.btn_upload_product);
 
         btn_menu = findViewById(R.id.btn_menu);
@@ -61,6 +77,8 @@ public class ListingActivity extends AppCompatActivity {
         description_product = findViewById(R.id.description_product);
         price_product = findViewById(R.id.price_product);
         iv_select_image = findViewById(R.id.iv_select_image);
+
+
 
         btn_menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,11 +108,16 @@ public class ListingActivity extends AppCompatActivity {
             }
         });
 
+
         btn_upload_product.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 UploadImage();
+
+                Toast.makeText(getApplicationContext(), "Product uploaded Successfully!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(ListingActivity.this, MenuActivity.class));
+                finish();
 
             }
         });
@@ -132,6 +155,7 @@ public class ListingActivity extends AppCompatActivity {
 
 
     public void UploadImage() {
+        System.out.println("user id: " + currentUser);
         name = name_product.getText().toString();
         description = description_product.getText().toString();
         price = price_product.getText().toString();
@@ -144,24 +168,23 @@ public class ListingActivity extends AppCompatActivity {
         } else if (FilePathUri == null) {
             Toast.makeText(ListingActivity.this, "Please add product image!", Toast.LENGTH_SHORT).show();
         } else {
-            progressDialog.setTitle("Product uploading...");
-            progressDialog.show();
+            // progressDialog.setTitle("Product uploading...");
+            // progressDialog.show();
+
             StorageReference storageReference2 = storageReference.child(System.currentTimeMillis() + "." + GetFileExtension(FilePathUri));
             storageReference2.putFile(FilePathUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
 
-
+                    // progressDialog.dismiss();
                     storageReference2.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
                         public void onSuccess(Uri uri) {
                             downloadLink = uri.toString();
-                            progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(), "Product uploaded Successfully ", Toast.LENGTH_LONG).show();
-                            System.out.println("download link: " + downloadLink);
-                            ProductInformation imageUploadInfo = new ProductInformation(name, downloadLink, description, price);
+                            ProductInformation imageUploadInfo = new ProductInformation(name, downloadLink, description, price, currentUser);
                             String ImageUploadId = databaseReference.push().getKey();
                             databaseReference.child(ImageUploadId).setValue(imageUploadInfo);
+
                         }
                     });
 
